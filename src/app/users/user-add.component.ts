4446c9575399fa000782dba200e71cdaec099065
CaioHTeixeira <caioteixeiraaa@gmail.com>
2022-08-30 15:36:17 -0300

    imageurl

--- src/app/users/user-add.component.ts
+++ src/app/users/user-add.component.ts
@@ -3,19 +3,26 @@ import { User } from "./user";
 import { UserService } from "./userService";
 
 @Component({
-    templateUrl: './user-add.component.html'
+    templateUrl: './user-add.component.html',
+    styleUrls: ['./user-add.component.css']
 })
 
 export class UserAddComponent {
 
     user: User = new User();
     
+    imgUrl: string = "../assets/images/default-user-image.png";
+
     constructor(private userService: UserService) {}
 
-    saveUser(): void {
+    saveUser(): void {   
         this.userService.saveUser(this.user).subscribe({
             next: user => console.log('User added with success', user),
             error: err => console.log('Error', err)
         })
+    }
+
+    saveImageUrl(event: Event): void {
+        this.user.imageUrl = ((<File>((event.target as HTMLInputElement).files as FileList)[0]) as File).webkitRelativePath;
     }
 }
